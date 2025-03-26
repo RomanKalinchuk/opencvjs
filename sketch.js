6f2b14cee406baea05208e84968e49c7a180b7b5
RomanKalinchuk <88523090+RomanKalinchuk@users.noreply.github.com>
2025-03-26 13:30:45 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,41 @@
-//Work in progress/incomplete
-const WIDTH = 600
-const HEIGHT = 500
-const BACKGROUND = 255
-const DIAMETER = WIDTH/20
-const ACC = 3
-const power = 0.2
-const instructions = " Arrows Left and Right for control "
-var grounded = false
-var velocity = ACC
-var ballX = 100
-var ballY = 50
+let src, dst, video, img, faceCascade
 
 function setup() {
-  createCanvas(600, 500)
-  frameRate(60)
-  stroke(0)
-  fill(0)
+  createCanvas(720, 480)
+  video = createCapture(VIDEO)
+  video.size(720, 480)
+  video.hide() // Hide the HTML video element
+  
 }
 
 function draw() {
-  background(BACKGROUND);
-  drawHill()
-  drawBall()
-  checkArrows()
+  background(50)
+  video.loadPixels()
+  grayscale(video)
+  image(video, 0,0,width,height)
+  
+  console.log(typeof cv)
+  
+}
+
+function grayscale(img){
+  for (let y = 0; y < video.height; y++) {
+    for (let x = 0; x < video.width; x++) {
+      let index = (x + y * video.width) * 4;
+      let r = img.pixels[index];
+      let g = img.pixels[index + 1];
+      let b = img.pixels[index + 2];
+      let grayed = (r + g + b) / 3; // Convert to grayscale
+
+      video.pixels[index] = grayed;
+      video.pixels[index + 1] = grayed;
+      video.pixels[index + 2] = grayed;
+      video.pixels[index + 3] = 255; // Set alpha to fully opaque
+    }
+  }
+  video.updatePixels()
+}
+
+function onOpenCvReady() {
+    console.log("OpenCV.js is ready!");
 }
