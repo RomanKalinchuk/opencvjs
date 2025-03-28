a7908b66a5112bd220cb700e6c9a717e45394740
RomanKalinchuk <88523090+RomanKalinchuk@users.noreply.github.com>
2025-03-27 20:55:22 -0400

    Update sketch.js

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -7,6 +7,7 @@ function setup() {
   video = createCapture(VIDEO)
   video.size(720, 480)
   video.hide() // Hide the HTML video element
+  while(!openCVReady);
   classifier = new cv.CascadeClassifier()
   classifier.load('haarcascade_frontalface_default.xml')
   faces = new cv.RectVector()
