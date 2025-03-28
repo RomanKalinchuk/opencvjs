f816d492778929fabba98372b9031a082c926c04
RomanKalinchuk <88523090+RomanKalinchuk@users.noreply.github.com>
2025-03-27 22:13:09 -0400

    Update sketch.js

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -7,9 +7,9 @@ function setup() {
   video = createCapture(VIDEO)
   video.size(720, 480)
   video.hide() // Hide the HTML video element
-  cv.then(() => {console.log(cv)})
+  cv.then((cv2) => {console.log(cv2)})
   //while(typeof cv.CascadeClassifier == 'undefined');
-  console.log('done!')
+  console.log('new!')
   //classifier = new cv.CascadeClassifier()
   //classifier.load('/opencvjs/opencv/haarcascade_frontalface_default.xml')
   //faces = new cv.RectVector()
