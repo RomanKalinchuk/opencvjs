8842a7e67e8e4a08d8d5f63efec17c81fe797592
RomanKalinchuk <88523090+RomanKalinchuk@users.noreply.github.com>
2025-03-27 22:18:23 -0400

    Update sketch.js

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -7,12 +7,13 @@ function setup() {
   video = createCapture(VIDEO)
   video.size(720, 480)
   video.hide() // Hide the HTML video element
-  cv.then((CV) => {cv = CV})
-  console.log(cv)
-  classifier = new cv.CascadeClassifier()
-  classifier.load('/opencvjs/opencv/haarcascade_frontalface_default.xml')
-  faces = new cv.RectVector()
-  gray = new cv.Mat()
+  cv.then((cv) => {
+    classifier = new cv.CascadeClassifier()
+    classifier.load('/opencvjs/opencv/haarcascade_frontalface_default.xml')
+    faces = new cv.RectVector()
+    gray = new cv.Mat()
+    console.log(cv)
+  })
 }
 
 function draw() {
