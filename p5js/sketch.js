2d4eb1e4ae5cb9200847155c048a0682a5128b25
RomanKalinchuk <88523090+RomanKalinchuk@users.noreply.github.com>
2025-03-27 20:06:11 -0400

    Remove grayscale function

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -16,21 +16,3 @@ function draw() {
   image(video, 0,0,width,height)
   
 }
-
-function grayscale(img){
-  for (let y = 0; y < video.height; y++) {
-    for (let x = 0; x < video.width; x++) {
-      let index = (x + y * video.width) * 4;
-      let r = img.pixels[index];
-      let g = img.pixels[index + 1];
-      let b = img.pixels[index + 2];
-      let grayed = (r + g + b) / 3; // Convert to grayscale
-
-      video.pixels[index] = grayed * ((frameColor == 'Red' || frameColor == 'Black') ? 1:0);
-      video.pixels[index + 1] = grayed * ((frameColor == 'Green' || frameColor == 'Black') ? 1:0);
-      video.pixels[index + 2] = grayed * ((frameColor == 'Blue' || frameColor == 'Black') ? 1:0);
-      video.pixels[index + 3] = 255; // Set alpha to fully opaque
-    }
-  }
-  video.updatePixels()
-}
