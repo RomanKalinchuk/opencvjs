4260d2836ba1c7c1593bd7ab3b619d7c7a21fa42
RomanKalinchuk <88523090+RomanKalinchuk@users.noreply.github.com>
2025-03-27 20:14:05 -0400

    Update sketch.js

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -1,18 +1,21 @@
 let src, dst, video, img, faceCascade
 let frameColor = 'Black'
+let faces = []
 
 function setup() {
   createCanvas(720, 480)
   video = createCapture(VIDEO)
   video.size(720, 480)
   video.hide() // Hide the HTML video element
-  
+  faces = new cv.RectVector()
+  gray = new cv.Mat()
 }
 
 function draw() {
-  background(50)
+  //background(50)
   video.loadPixels()
-  grayscale(video)
+  grayscale()
   image(video, 0,0,width,height)
+  getFaces()
   
 }
