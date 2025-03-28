c9adceeca36d5d6d235a365f6c08c1be66b6f4db
RomanKalinchuk <88523090+RomanKalinchuk@users.noreply.github.com>
2025-03-28 18:05:54 -0400

    Update sketch.js

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -3,25 +3,11 @@ let frameColor = 'Black'
 let faceDetectReady = false;
 let faces = []
 
-function preload() {
-    console.log(cv.Mat)
-    // Wait until OpenCV.js is fully loaded
-    cv.onRuntimeInitialized = () => {
-        console.log("OpenCV.js is ready!");
-
-        let cascadeUrl = 'opencv/haarcascade_frontalface_default.xml';
-
-        // Preload Haar cascade XML file into OpenCV's filesystem
-        cv.FS_createPreloadedFile('/', cascadeUrl, cascadeUrl, true, false, () => {
-            faceCascade = new cv.CascadeClassifier();
-            faceCascade.load(cascadeUrl);
-            faceDetectReady = true;
-            console.log("Haar cascade loaded successfully");
-        }, (err) => {
-            console.error("Failed to load Haar cascade:", err);
-        });
-    };
-}
+async function loadCV() {
+    cv = (cv instanceof Promise) ? await cv : cv
+    faceDetectReady = true
+    console.log(Obect.keys(cv))
+}()
 
 function setup() {
   createCanvas(720, 480)
