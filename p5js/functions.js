a828efb1f24f53f511d7709966b8ab3411246a09
RomanKalinchuk <88523090+RomanKalinchuk@users.noreply.github.com>
2025-03-27 20:05:39 -0400

    Update functions.js

--- p5js/functions.js
+++ p5js/functions.js
@@ -5,3 +5,16 @@ function onOpenCvReady(){
 function setColor(color){
   frameColor = color
 }
+
+function getFaces(frame){
+  // Detect faces
+    classifier.detectMultiScale(gray, faces, 1.1, 3, 0);
+
+    // Draw rectangles around faces
+    for (let i = 0; i < faces.size(); ++i) {
+      let face = faces.get(i);
+      stroke(255, 0, 0);
+      noFill();
+      rect(face.x, face.y, face.width, face.height);
+    }
+}
